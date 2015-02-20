bca126643bfb15ae2770b22c8d5fc667e865a724
hackiosa <hackiosa@yahoo.de>
2015-02-20 22:22:06 +0100

    Huge cpu update

--- 6502.c
+++ 6502.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Frederic Meyer
+ * Copyright (C) 2015 Frederic Meyer
  * 
  * This file is part of gnes.
  *
@@ -958,63 +958,480 @@ void lda_idy()
     pc += 2;
 }
 
+void ldx_imm()
+{
+	x = mmu_read(pc + 1);
+	update_zero(x);
+	update_negative(x);
+	pc += 2;
+}
+
+void ldx_zp()
+{
+    x = mmu_read(mmu_read(pc + 1));
+    update_zero(x);
+    update_negative(x);
+    pc += 2;
+}
+
+void ldx_zpy()
+{
+    x = mmu_read((mmu_read(pc + 1) + y) & 0xFF);
+    update_zero(x);
+    update_negative(x);
+    pc += 2;
+}
+
+void ldx_abs()
+{
+    x = mmu_read(mmu_read16(pc + 1));
+    update_zero(x);
+    update_negative(x);
+    pc += 3;
+}
+
+void ldx_aby()
+{
+    uint16_t address = mmu_read16(pc + 1) + y;
+    x = mmu_read(address);
+    update_zero(x);
+    update_negative(x);
+    if ((address & 0xFF00) != (pc & 0xFF00))
+        cycles++; 
+    pc += 3;
+}
+
+void ldy_imm()
+{
+	y = mmu_read(pc + 1);
+	update_zero(y);
+	update_negative(y);
+	pc += 2;
+}
+
+void ldy_zp()
+{
+    y = mmu_read(mmu_read(pc + 1));
+    update_zero(y);
+    update_negative(y);
+    pc += 2;
+}
+
+void ldy_zpx()
+{
+    y = mmu_read((mmu_read(pc + 1) + x) & 0xFF);
+    update_zero(y);
+    update_negative(y);
+    pc += 2;
+}
+
+void ldy_abs()
+{
+    y = mmu_read(mmu_read16(pc + 1));
+    update_zero(y);
+    update_negative(y);
+    pc += 3;
+}
+
+void ldy_abx()
+{
+    uint16_t address = mmu_read16(pc + 1) + x;
+    y = mmu_read(address);
+    update_zero(y);
+    update_negative(y);
+    if ((address & 0xFF00) != (pc & 0xFF00))
+        cycles++; 
+    pc += 3;
+}
+
+void lsr_acc()
+{
+    if (a & 1) set_carry();
+        else clear_carry();
+    a >>= 1;
+    update_zero(a);
+    update_negative(a);
+    pc++;
+}
+
+void lsr_zp()
+{
+    uint32_t address = mmu_read(pc + 1);
+    uint8_t value = mmu_read(address);
+    if (value & 1) set_carry();
+        else clear_carry();
+    value >>= 1;
+    update_zero(value);
+    update_negative(value);
+    mmu_write(address, value);
+    pc += 2;
+}
+
+void lsr_zpx()
+{
+    uint32_t address = (mmu_read(pc + 1) + x) & 0xFF;
+    uint8_t value = mmu_read(address);
+    if (value & 1) set_carry();
+        else clear_carry();
+    value >>= 1;
+    update_zero(value);
+    update_negative(value);
+    mmu_write(address, value);
+    pc += 2;
+}
+
+void lsr_abs()
+{
+    uint32_t address = mmu_read16(pc + 1);
+    uint8_t value = mmu_read(address);
+    if (value & 1) set_carry();
+        else clear_carry();
+    value >>= 1;
+    update_zero(value);
+    update_negative(value);
+    mmu_write(address, value);
+    pc += 3;
+}
+
+void lsr_abx()
+{
+    uint32_t address = mmu_read16(pc + 1) + x;
+    uint8_t value = mmu_read(address);
+    if (value & 1) set_carry();
+        else clear_carry();
+    value >>= 1;
+    update_zero(value);
+    update_negative(value);
+    mmu_write(address, value);
+    pc += 3;
+}
+
+void nop_imp() 
+{
+    pc++;
+}
+
+void ora_imm()
+{
+    a |= mmu_read(pc + 1);
+    update_zero(a);
+    update_negative(a);
+    pc += 2;
+}
+
+void ora_zp()
+{
+    a |= mmu_read(mmu_read(pc + 1));
+    update_zero(a);
+    update_negative(a);
+    pc += 2;
+}
+
+void ora_zpx()
+{
+    a |= mmu_read(mmu_read(pc + 1) + x);
+    update_zero(a);
+    update_negative(a);
+    pc += 2;
+}
+
+void ora_abs()
+{
+    a |= mmu_read(mmu_read16(pc + 1));
+    update_zero(a);
+    update_negative(a);
+    pc += 3;
+}
+
+void ora_abx()
+{
+    uint32_t address = mmu_read16(pc + 1) + x;
+    a |= mmu_read(address);
+    if ((address & 0xFF00) != (pc & 0xFF00))
+        cycles++; 
+    pc += 3;
+}
+
+void ora_aby()
+{
+    uint32_t address = mmu_read16(pc + 1) + y;
+    a |= mmu_read(address);
+    if ((address & 0xFF00) != (pc & 0xFF00))
+        cycles++; 
+    pc += 3;
+}
+
+void ora_idx()
+{
+    a |= mmu_read(mmu_read16(mmu_read(pc + 1) + x));
+    update_zero(a);
+    update_negative(a);
+    pc += 2;
+}
+
+void ora_idy()
+{
+    uint16_t address = mmu_read16(mmu_read(pc + 1)) + y;
+    a |= mmu_read(address);
+    update_zero(a);
+    update_negative(a);
+    if ((address & 0xFF00) != (pc & 0xFF00))
+        cycles++; 
+    pc += 2;
+}
+
+void pha_imp()
+{
+    cpu_push(a);
+    pc++;
+}
+
+void php_imp()
+{
+    cpu_push(p);
+    pc++;
+}
+
+void pla_imp()
+{
+    a = cpu_pop();
+    pc++;
+}
+
+void plp_imp()
+{
+    p = cpu_pop();
+    pc++;
+}
+
+void rol_acc()
+{
+    uint32_t carry = CARRY;
+    if (a & 7) set_carry();
+        else clear_carry();
+    a = (a << 1) | carry;
+    update_zero(a);
+    update_negative(a);
+    pc++;
+}
+
+void rol_zp()
+{
+    uint32_t address = mmu_read(pc + 1);
+    uint8_t value = mmu_read(address);
+    uint32_t carry = CARRY;
+    if (value & 7) set_carry();
+        else clear_carry();
+    value = (value << 1) | carry;
+    update_zero(value);
+    update_negative(value);
+    mmu_write(address, value);
+    pc += 2;
+}
+
+void rol_zpx()
+{
+    uint32_t address = (mmu_read(pc + 1) + x) & 0xFF;
+    uint8_t value = mmu_read(address);
+    uint32_t carry = CARRY;
+    if (value & 7) set_carry();
+        else clear_carry();
+    value = (value << 1) | carry;
+    update_zero(value);
+    update_negative(value);
+    mmu_write(address, value);
+    pc += 2;
+}
+
+void rol_abs()
+{
+    uint32_t address = mmu_read16(pc + 1);
+    uint8_t value = mmu_read(address);
+    uint32_t carry = CARRY;
+    if (value & 7) set_carry();
+        else clear_carry();
+    value = (value << 1) | carry;
+    update_zero(value);
+    update_negative(value);
+    mmu_write(address, value);
+    pc += 3;
+}
+
+void rol_abx()
+{
+    uint32_t address = mmu_read16(pc + 1) + x;
+    uint8_t value = mmu_read(address);
+    uint32_t carry = CARRY;
+    if (value & 7) set_carry();
+        else clear_carry();
+    value = (value << 1) | carry;
+    update_zero(value);
+    update_negative(value);
+    mmu_write(address, value);
+    pc += 3;
+}
+
+void ror_acc()
+{
+    uint32_t carry = CARRY;
+    if (a & 0) set_carry();
+        else clear_carry();
+    a = (a >> 1) | (carry << 7);
+    update_zero(a);
+    update_negative(a);
+    pc++;
+}
+
+void ror_zp()
+{
+    uint32_t address = mmu_read(pc + 1);
+    uint8_t value = mmu_read(address);
+    uint32_t carry = CARRY;
+    if (value & 0) set_carry();
+        else clear_carry();
+    value = (value >> 1) | (carry << 7);
+    update_zero(value);
+    update_negative(value);
+    mmu_write(address, value);
+    pc += 2;
+}
+
+void ror_zpx()
+{
+    uint32_t address = (mmu_read(pc + 1) + x) & 0xFF;
+    uint8_t value = mmu_read(address);
+    uint32_t carry = CARRY;
+    if (value & 0) set_carry();
+        else clear_carry();
+    value = (value >> 1) | (carry << 7);
+    update_zero(value);
+    update_negative(value);
+    mmu_write(address, value);
+    pc += 2;
+}
+
+void ror_abs()
+{
+    uint32_t address = mmu_read16(pc + 1);
+    uint8_t value = mmu_read(address);
+    uint32_t carry = CARRY;
+    if (value & 0) set_carry();
+        else clear_carry();
+    value = (value >> 1) | (carry << 7);
+    update_zero(value);
+    update_negative(value);
+    mmu_write(address, value);
+    pc += 3;
+}
+
+void ror_abx()
+{
+    uint32_t address = mmu_read16(pc + 1) + x;
+    uint8_t value = mmu_read(address);
+    uint32_t carry = CARRY;
+    if (value & 0) set_carry();
+        else clear_carry();
+    value = (value >> 1) | (carry << 7);
+    update_zero(value);
+    update_negative(value);
+    mmu_write(address, value);
+    pc += 3;
+}
+
 void cpu_step()
 {
     switch (mmu_read(pc))
     {
     case 0x00: brk(); break;
+    case 0x01: ora_idx(); break;
+    case 0x05: ora_zp(); break;
     case 0x06: asl_zp(); break;
+    case 0x08: php_imp(); break;
+    case 0x09: ora_imm(); break;
     case 0x0A: asl_acc(); break;
+    case 0x0D: ora_abs(); break;
     case 0x0E: asl_abs(); break;
     case 0x10: bpl_rel(); break;
+    case 0x11: ora_idy(); break;
+    case 0x15: ora_zpx(); break;
     case 0x16: asl_zpx(); break;
     case 0x18: clc(); break;
+    case 0x19: ora_aby(); break;
+    case 0x1D: ora_abx();
     case 0x1E: asl_abx(); break;
     case 0x20: jsr_abs(); break;
     case 0x21: and_idx(); break;
     case 0x24: bit_zp(); break;
     case 0x25: and_zp(); break;
+    case 0x26: rol_zp(); break;
+    case 0x28: plp_imp(); break;
     case 0x29: and_imm(); break;
+    case 0x2A: rol_acc(); break;
     case 0x2C: bit_abs(); break;
     case 0x2D: and_abs(); break;
+    case 0x2E: rol_abs(); break;
     case 0x30: bmi_rel(); break;
     case 0x31: and_idy(); break;
     case 0x35: and_zpx(); break;
+    case 0x36: rol_zpx(); break;
     case 0x39: and_aby(); break;
     case 0x3D: and_abx(); break;
+    case 0x3E: rol_abx(); break;
     case 0x41: eor_idx(); break;
     case 0x45: eor_zp(); break;
+    case 0x46: lsr_zp(); break;
+    case 0x48: pha_imp(); break;
     case 0x49: eor_imm(); break;
+    case 0x4A: lsr_acc(); break;
     case 0x4C: jmp_abs(); break;
     case 0x4D: eor_abs(); break;
+    case 0x4E: lsr_abs(); break;
     case 0x50: bvc_rel(); break;
     case 0x51: eor_idy(); break;
     case 0x55: eor_zpx(); break;
+    case 0x56: lsr_zpx(); break;
     case 0x58: cli(); break;
     case 0x59: eor_aby(); break;
     case 0x5D: eor_abx(); break;
+    case 0x5E: lsr_abx(); break;
     case 0x61: adc_idx(); break;
     case 0x65: adc_zp(); break;
+    case 0x66: ror_zp(); break;
+    case 0x68: pla_imp(); break;
     case 0x69: adc_imm(); break;
+    case 0x6A: ror_acc(); break;
     case 0x6C: jmp_ind(); break;
     case 0x6D: adc_abs(); break;
+    case 0x6E: ror_abs(); break;
     case 0x70: bvs_rel(); break;
     case 0x71: adc_idy(); break;
     case 0x75: adc_zpx(); break;
+    case 0x76: ror_zpx(); break;
     case 0x79: adc_aby(); break;
     case 0x7D: adc_abx(); break;
+    case 0x7E: ror_abx(); break;
     case 0x88: dey(); break;
     case 0x90: bcc_rel(); break;
+    case 0xA0: ldy_imm(); break;
     case 0xA1: lda_idx(); break;
+    case 0xA2: ldx_imm(); break;
+    case 0xA4: ldy_zp(); break;
     case 0xA5: lda_zp(); break;
+    case 0xA6: ldx_zp(); break;
     case 0xA9: lda_imm(); break;
+    case 0xAC: ldy_abs(); break;
     case 0xAD: lda_abs(); break;
+    case 0xAE: ldx_abs(); break;
     case 0xB0: bcs_rel(); break;
     case 0xB1: lda_idy(); break;
+    case 0xB4: ldy_zpx(); break;
     case 0xB5: lda_zpx(); break;
+    case 0xB6: ldx_zpy(); break;
     case 0xB8: clv(); break;
     case 0xB9: lda_aby(); break;
+    case 0xBC: ldy_abx(); break;
     case 0xBD: lda_abx(); break;
+    case 0xBE: ldx_aby(); break;
     case 0xC0: cpy_imm(); break;
     case 0xC1: cmp_idx(); break;
     case 0xC4: cpy_zp(); break;
@@ -1037,6 +1454,7 @@ void cpu_step()
     case 0xE4: cpx_zp(); break;
     case 0xE6: inc_zp(); break;
     case 0xE8: inx(); break;
+    case 0xEA: nop_imp(); break;
     case 0xEC: cpx_abs(); break;
     case 0xEE: inc_abs(); break;
     case 0xF0: beq_rel(); break;
